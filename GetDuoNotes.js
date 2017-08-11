9a8d87a080d9ab1ffbc77a6b0a247182f3682ec5
Anedds <30927428+Anedds@users.noreply.github.com>
2017-08-11 08:24:43 -0300

    Update GetDuoNotes.js

--- GetDuoNotes.js
+++ GetDuoNotes.js
@@ -1,1 +1,59 @@
 $("body").html('<div id="words">');var ld=duo.user.attributes.language_data;var notes=[];var zeroes="000";for(l in ld){ld[l].skills.models.forEach(function(e){var t=e.attributes;if(t.has_explanation==true){notes.push("<!-- "+zeroes.substr(0,3-t.coords_y.toString().length)+t.coords_y.toString()+" --><h1>"+t.name+"</h1>\n<div>"+t.explanation+"</div><br><br>");}})}notes=notes.sort();for(k in notes){if(k>0){$("#words").append("<hr>");}$("#words").append(notes[k]);}$("body").append("</div>")
+GET 
+https://www.googletagservices.com/dcm/impl_v39.js [HTTP/2.0 200 OK 167 ms]
+GET 
+https://ff.kis.v2.scr.kaspersky-labs.com/A89D6520-91A1-D04C-9545-33E544ABE2B7/websocket [HTTP/1.1 101  6 ms]
+GET 
+https://ad.doubleclick.net/ddm/adi/N5295.164403.ADWORDS.GOOGLE.COM/B20125611.201905659;dc_ver=39.103;dc_eid=40004001;sz=300x250;u_sd=1;dc_adk=3372583235;ord=3dk5av;click=https%3A%2F%2Fwww.googleadservices.com%2Fpagead%2Faclk%3Fsa%3DL%26ai%3DCY3NdZpCNWbujLtePxgTWg6D4CMW2nJ9LtvG9vLQF3baYitAIEAEgzbjVKGDN0OWAkAOgAcuw2ZsDyAEJqAMBqgSGAU_Qch3t8zkj7YIihMm445nBFD3nAuwfGNK40cEO9zlYTu6qe9fuvhnq_hmkazKu_WtFKQPJfLYKi3Ki1fZbTWegxM2LlTmSBFsuHqk4W9kyTMXNwZcqx_JyW5maXytoXwwLv5rU1Tu4xdS7bk0QAIFXr5vds04g3KWazbrXwukFUNBGh9UVoAYRgAedz6ZkqAevyhuoB93KG6gHpr4b2AcA0ggHCIBhEAEYAtgTAg%26num%3D1%26cid%3DCAASEuRoJN1Sp58sS1R1eYHkSYSS0Q%26sig%3DAOD64_3pchk6aUvzgck5OzOAu9DhFDjVNQ%26client%3Dca-pub-2200036119221572%26adurl%3D;dc_rfl=2,,https%3A%2F%2Fwww.duolingo.com%2F$0;xdt=1;crlt=*jhB2RDGA_;osda=1;sttr=252;prcl=s [HTTP/2.0 200 OK 1321 ms]
+GET 
+https://cm.g.doubleclick.net/push [HTTP/2.0 204 No Content 930 ms]
+GET 
+https://tpc.googlesyndication.com/pagead/images/transparent.png [HTTP/2.0 200 OK 153 ms]
+GET 
+https://www.google.com/ads/measurement/l [HTTP/2.0 204 No Content 267 ms]
+GET 
+https://ff.kis.v2.scr.kaspersky-labs.com/A89D6520-91A1-D04C-9545-33E544ABE2B7/main.js [HTTP/1.1 200  16 ms]
+GET 
+https://s0.2mdn.net/879366/html_inpage_rendering_lib_200_194.js [HTTP/2.0 200 OK 386 ms]
+GET 
+https://tpc.googlesyndication.com/sodar/d5qAyLYU.js [HTTP/2.0 200 OK 123 ms]
+GET 
+https://ff.kis.v2.scr.kaspersky-labs.com/A89D6520-91A1-D04C-9545-33E544ABE2B7/websocket [HTTP/1.1 101  0 ms]
+GET 
+https://tpc.googlesyndication.com/sodar/9im3l02I.html [HTTP/2.0 200 OK 129 ms]
+GET 
+https://ff.kis.v2.scr.kaspersky-labs.com/A89D6520-91A1-D04C-9545-33E544ABE2B7/main.js [HTTP/1.1 200  12 ms]
+GET 
+https://pagead2.googlesyndication.com/bg/-_aCScfq4sp9ogrAPq48ybR157SjacgjRbwiGDxWcsU.js [HTTP/2.0 200 OK 119 ms]
+GET 
+https://ff.kis.v2.scr.kaspersky-labs.com/A89D6520-91A1-D04C-9545-33E544ABE2B7/websocket [HTTP/1.1 101  0 ms]
+GET 
+https://pagead2.googlesyndication.com/pagead/js/lidar.js [HTTP/2.0 200 OK 127 ms]
+GET 
+https://s0.2mdn.net/ads/richmedia/studio/pv2/60049233/20170123131342247/index.html [HTTP/2.0 200 OK 619 ms]
+GET 
+https://googleads4.g.doubleclick.net/pcs/view [HTTP/2.0 200 OK 297 ms]
+GET 
+https://ff.kis.v2.scr.kaspersky-labs.com/A89D6520-91A1-D04C-9545-33E544ABE2B7/main.js [HTTP/1.1 200  16 ms]
+GET 
+https://s0.2mdn.net/879366/Enabler_01_152.js [HTTP/2.0 200 OK 726 ms]
+GET 
+https://ff.kis.v2.scr.kaspersky-labs.com/A89D6520-91A1-D04C-9545-33E544ABE2B7/websocket [HTTP/1.1 101  22 ms]
+GET 
+https://pagead2.googlesyndication.com/pagead/gen_204 [HTTP/2.0 204 No Content 373 ms]
+GET 
+https://s0.2mdn.net/ads/richmedia/studio/pv2/60049233/20170123131342247/backup.jpg [HTTP/2.0 200 OK 282 ms]
+GET 
+https://ade.googlesyndication.com/ddm/activity/dc_oe=ChMIt4GqyIXP1QIVRoKRCh1wGQtKEAAYACCqwdAl;met=1;&timestamp=1502449738300;eid1=871060;ecn1=1;etm1=0;eid2=2;ecn2=1;etm2=10; [HTTP/2.0 200 OK 487 ms]
+GET 
+https://ade.googlesyndication.com/ddm/activity/dc_oe=ChMIt4GqyIXP1QIVRoKRCh1wGQtKEAAYACCqwdAl;met=1;&timestamp=1502449748296;eid1=2;ecn1=0;etm1=10; [HTTP/2.0 200 OK 225 ms]
+GET 
+https://ade.googlesyndication.com/ddm/activity/dc_oe=ChMIt4GqyIXP1QIVRoKRCh1wGQtKEAAYACCqwdAl;met=1;&timestamp=1502449778297;eid1=2;ecn1=0;etm1=30; [HTTP/2.0 200 OK 338 ms]
+GET 
+https://ade.googlesyndication.com/ddm/activity/dc_oe=ChMIt4GqyIXP1QIVRoKRCh1wGQtKEAAYACCqwdAl;met=1;&timestamp=1502449848296;eid1=2;ecn1=0;etm1=70; [HTTP/2.0 200 OK 295 ms]
+GET 
+https://d35aaqx5ub95lt.cloudfront.net/css/ltr-bf009ea0.css [HTTP/2.0 200 OK 0 ms]
+GET 
+https://ade.googlesyndication.com/ddm/activity/dc_oe=ChMIt4GqyIXP1QIVRoKRCh1wGQtKEAAYACCqwdAl;met=1;&timestamp=1502449968304;eid1=2;ecn1=0;etm1=120; [HTTP/2.0 200 OK 156 ms]
+
+        
